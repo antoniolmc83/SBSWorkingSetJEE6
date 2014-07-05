a32d9e12dd64b7c6bc48e5b759afeeaa1ff15834
Antonio Merejildo <antoniolmc83@gmail.com>
2014-07-05 18:28:20 -0500

    Resuming work

--- sbs-parent/SBSPortalScheduler/src/test/java/pe/almc/sbs/SBSTasasFacadeTestCase.java
+++ sbs-parent/SBSPortalScheduler/src/test/java/pe/almc/sbs/SBSTasasFacadeTestCase.java
@@ -2,15 +2,21 @@ package pe.almc.sbs;
 
 
 
+import java.util.Date;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 import pe.almc.sbs.service.SBSTasasFacade;
 
 public class SBSTasasFacadeTestCase {
-
+	private final static Logger logger = LoggerFactory.getLogger(SBSTasasFacadeTestCase.class);
 	private SBSTasasFacade sbsTasasFacade;
 	
 	public SBSTasasFacadeTestCase() {
@@ -40,6 +46,49 @@ public class SBSTasasFacadeTestCase {
 	public void testObtenerTasas() {
 		String resp = sbsTasasFacade.obtenerTasas();
 		TestCase.assertEquals("0", resp);
+	}
+	
+	@Test
+	public void testMetodoJob(){
+		//Regiones
+		if( sbsTasasFacade.regionFindAll().isEmpty() ){
+			logger.info("No hay regiones... inicia proceso de registro de regiones");
+			String rsp = sbsTasasFacade.obtenerRegiones();
+			if("0".equals(rsp)){
+				logger.info("Se completo registro de regiones");
+			}else{
+				logger.info("Error en registro de regiones");
+			}
+		}
+		
+		//Productos
+		if( sbsTasasFacade.productoFindAll().isEmpty() ){
+			logger.info("No hay productos... inicia proceso de registro de regiones");
+			String rsp = sbsTasasFacade.obtenerProductos();
+			if("0".equals(rsp)){
+				logger.info("Se completo registro de productos");
+			}else{
+				logger.info("Error en registro de productos");
+			}
+			
+		}		
+		
+		//Condiciones
+		if( sbsTasasFacade.condicionFindAll().isEmpty() ){
+			logger.info("No hay condiciones... inicia proceso de registro de regiones");
+			String rsp = sbsTasasFacade.obtenerCondiciones();
+			if("0".equals(rsp)){
+				logger.info("Se completo registro de condiciones");
+			}else{
+				logger.info("Error en registro de condiciones");
+			}			
+		}
+		
+		
+		logger.info("Iniciando registro de tasas diarias " +  new Date());
+		sbsTasasFacade.obtenerTasas();
+		logger.info("Finalizando " +  new Date());
+
 	}
 
 }
